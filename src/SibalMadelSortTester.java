c2d665fc34f365966071740e5ed4c171763486d3
madelds <sibalmadel@gmail.com>
2023-11-01 23:42:18 -0500

    Add Tests for Personal Sort

---
+++ src/SibalMadelSortTester.java
@@ -0,0 +1,17 @@
+/**
+ * Tests of SibalMadelSort.
+ * 
+ * @author Madel Sibal & Samuel A. Rebelsky
+ */
+public class SibalMadelSortTester extends SortTester {
+
+  // +--------------+------------------------------------------------
+  // | Constructors |
+  // +--------------+
+
+  public SibalMadelSortTester() {
+    super();
+    this.sorter = SibalMadelSort.SORTER;
+  } // SibalMadelSortTester()
+
+} // class SibalMadelSortTester
